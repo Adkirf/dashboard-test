4dfd72dd6123672bd3ffcede199c45fc2a260101
Adkirf <adkirfuglyape@gmail.com>
2023-03-27 15:30:40 +0200

    first commit

---
+++ server/routes/auth.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+
+
+//REGISTER
+router.post("/register", async (req, res) => {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword,
+        });
+
+        const user = await newUser.save();
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// LOGIN 
+router.post("/login", async (req, res)=> {
+    try {
+        const user = await User.findOne({username: req.body.username})
+        !user && res.status(400).json("wrong credentials!") /// if there is no user then send status 400 wrong credential
+
+        
+        const validated =  req.body.password === user.password 
+        !validated && res.status(400).json("wrong credentials!")
+        
+        const jwtToken = jwt.sign(
+            {username: user.username},
+            process.env.JWT_SECRET,
+          );
+
+        res.status(200).json({message: "Welcome back!", token: jwtToken}) /// id there is any user according to the database ten send status 200 is successful
+    }catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
+module.exports = router;
